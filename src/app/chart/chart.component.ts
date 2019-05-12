e0ecc3c158404fc91b788b597d8b9449635095a7
rzepaw <rzepaw@gmail.com>
2019-05-12 18:56:32 +0200

    init

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -40,32 +40,34 @@ export class ChartComponent implements OnInit {
       .append('g')
       .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
 
-    const radius = Math.min(width, height) / 2 - margin.left;
+    const circle = svg.append('circle')
+      .attr('cx', 30)
+      .attr('cy', 30)
+      .attr('r', 20);
 
-    const data: Value[] = [new Value('a', 10), new Value('b', 20), new Value('c', 30)];
-
-    // set the color scale
-    var color = d3.scaleOrdinal()
-      .domain(data)
-      .range(['#98abc5', '#8a89a6', '#7b6888', '#6b486b', '#a05d56']);
-
-    // Compute the position of each group on the pie:
-    var pie = d3.pie().value(function(d: Value) {
-      return d.value;
-    });
-    var data_ready = pie(d3.entries(data));
-
-// Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
-    svg
-      .selectAll('whatever')
-      .data(data_ready)
-      .enter()
-      .append('path')
-      .attr('d', d3.arc()
-        .innerRadius(0)
-        .outerRadius(radius)
-      )
-      .attr('fill', d => (color(d.data.key)));
-  };
-
+    // const radius = Math.min(width, height) / 2 - margin.left;
+    //
+    // const data: Value[] = [new Value('a', 10), new Value('b', 20), new Value('c', 30)];
+    //
+    // // set the color scale
+    // const color = d3.scaleOrdinal()
+    //   .domain(data)
+    //   .range(['#98abc5', '#8a89a6', '#7b6888', '#6b486b', '#a05d56']);
+    //
+    // // Compute the position of each group on the pie:
+    // const pie = d3.pie().value((d: Value): number => d.value);
+    // const dataReady = pie(d3.entries(data));
+    //
+    // // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
+    // svg
+    //   .selectAll('whatever')
+    //   .data(dataReady)
+    //   .enter()
+    //   .append('path')
+    //   .attr('d', d3.arc()
+    //     .innerRadius(0)
+    //     .outerRadius(radius)
+    //   )
+    //   .attr('fill', d => (color(d.data.key)));
+  }
 }
