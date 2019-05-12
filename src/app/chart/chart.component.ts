2f2e4bc6b4c12b6f5fb74dbbe148b3edfe14a7d2
rzepaw <rzepaw@gmail.com>
2019-05-12 22:09:22 +0200

    pie chart

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -2,13 +2,24 @@ import {Component, OnInit} from '@angular/core';
 import * as d3 from 'd3';
 import {Value} from './value';
 
-
 @Component({
   selector: 'app-chart',
   templateUrl: './chart.component.html',
   styleUrls: ['./chart.component.sass']
 })
 export class ChartComponent implements OnInit {
+
+  data: Value[] = [
+    new Value('Win 2× / 47%', 15, '#98abc5'),
+    new Value('Win 3× / 31%', 10, '#8a89a6'),
+    new Value('Win 5× / 19%', 6, '#7b6888'),
+    new Value('Win 30× / 3%', 1, '#6b486b')
+  ];
+
+  width = 100;
+  height = 100;
+  outerRadius = Math.min(this.width, this.height) / 3;
+  innerRadius = this.outerRadius / 2;
 
   constructor() {
   }
@@ -18,58 +29,40 @@ export class ChartComponent implements OnInit {
   }
 
   createChart() {
+    const g = this.prepareG('chart');
+    this.pie(g);
+  }
 
-    const data: Value[] = [
-      new Value('Win 2x', 15, '#98abc5'),
-      new Value('Win 3x', 10, '#8a89a6'),
-      new Value('Win 5x', 6, '#7b6888'),
-      new Value('Win 30x', 1, '#6b486b')];
-
-    const width = 800;
-    const height = 800;
-    const outerRadius = Math.min(width, height) / 3;
-    const innerRadius = outerRadius / 2;
-
-    const margin = {
-      left: 10,
-      right: 10,
-      top: 10,
-      bottom: 10
-    };
-
-    d3.selectAll('#chart svg').remove();
-
-    const svg = d3.select('#chart')
+  private prepareG(id: string) {
+    d3.selectAll(`#${id} svg`).remove();
+    return d3.select(`#${id}`)
       .append('svg')
       .attr('width', '100%')
       .attr('height', '100%')
-      .attr('viewBox', '0 0 ' + width + ' ' + height)
+      .attr('viewBox', '0 0 ' + this.width + ' ' + this.height)
       .attr('perserveAspectRatio', 'xMidYMid')
       .append('g')
-      .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
+      .attr('transform', 'translate(' + this.width / 2 + ',' + this.height / 2 + ')');
+  }
 
-    const pie = d3.pie().value((d: {key: string, value: Value}): number => {
-      console.log('D', d);
+  private pie(g) {
+    const pie = d3.pie().value((d: { key: string, value: Value }): number => {
       return d.value.value;
     });
     console.log('Pie', pie);
 
-    const dataReady = pie(d3.entries(data));
+    const dataReady = pie(d3.entries(this.data));
     console.log('Data ready', dataReady);
 
-    svg
-      .selectAll('whatever')
+    g.selectAll('whatever')
       .data(dataReady)
       .enter()
       .append('path')
       .attr('d', d3.arc()
-        .innerRadius(innerRadius)
-        .outerRadius(outerRadius)
+        .innerRadius(this.innerRadius)
+        .outerRadius(this.outerRadius)
       )
-      .attr('fill', (d: {data: {key: string, value: Value}}): string => {
-        console.log('D', d);
-        console.log('D.data', d.data.value);
-        console.log('D.data.color', d.data.value.color);
+      .attr('fill', (d: { data: { key: string, value: Value } }): string => {
         return d.data.value.color;
       });
   }
