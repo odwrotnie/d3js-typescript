c9b4b4b830d8dc09132d7addc8f0edd113f1278c
rzepaw <rzepaw@gmail.com>
2019-05-25 13:58:45 +0200

    pie chart

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -10,39 +10,95 @@ import {Value} from './value';
 export class ChartComponent implements OnInit {
 
   data: Value[] = [
-    new Value('Win 2× / 47%', 15, '#98abc5'),
-    new Value('Win 3× / 31%', 10, '#8a89a6'),
-    new Value('Win 5× / 19%', 6, '#7b6888'),
-    new Value('Win 30× / 3%', 1, '#6b486b')
+    new Value('Win 2× / 47%', 15, '#D6A6F1'),
+    new Value('Win 3× / 31%', 10, '#992EFB'),
+    new Value('Win 5× / 19%', 6, '#7519B5'),
+    new Value('Win 30× / 3%', 1, '#E51DE6')
   ];
 
-  width = 100;
-  height = 100;
+  width = 1000;
+  height = 1000;
   outerRadius = Math.min(this.width, this.height) / 3;
   innerRadius =  this.outerRadius / 2;
 
   constructor() {
   }
 
+  transformRotate(angle: number): string {
+    return `translate(${this.width / 2}, ${this.height / 2}) rotate(${angle})`;
+  }
+
   ngOnInit() {
-    this.createChart();
+    const svg = this.prepareSVG('pie');
+    const gPie = svg
+      .append('g')
+      .attr('transform', this.transformRotate(0));
+    const gGadgets = svg
+      .append('g')
+      .attr('transform', this.transformRotate(0));
+    const infoG = d3.select(`#info`).style('padding', '1em');
+    this.pie(gPie);
+
+    gPie.append('polygon')
+      .attr('points', '100,10 40,198 190,78 10,78 160,198')
+      .style('fill', 'lime')
+      .style('stroke', 'purple');
+
+    infoG.append('text')
+      .attr('id', 'state')
+      .attr('text-anchor', 'end')
+      .attr('alignment-baseline', 'middle')
+      .attr('x', 0).attr('y', 0)
+      .style('fill', '#a62020')
+      .text('?');
+
+    let state = 0;
+    const desiredAngle = 290;
+    const timeLimit = 3000;
+    const t = d3.timer((duration: number) => {
+
+      const angle = duration / 10;
+      const angle360 = angle % 360;
+
+      if (state <= 0 && (duration <= timeLimit || angle360 > desiredAngle)) {
+        state = 0;
+      } else if (state <= 1 && angle360 <= desiredAngle) {
+        state = 1;
+      } else {
+        state = 2;
+      }
+
+      console.log('State', state);
+      d3.select('#state').text(`S:${state} T:${Math.round(duration)} A:${Math.round(angle360)}`);
+
+      switch (state) {
+        case 0:
+          gPie.attr('transform', this.transformRotate(angle));
+          break;
+        case 1:
+          gPie.attr('transform', this.transformRotate(angle));
+          break;
+        case 2:
+          gPie.attr('transform', this.transformRotate(desiredAngle));
+          t.stop();
+          break;
+      }
+
+      // g.transition()
+      //   .duration(100)
+      //   .attr('transform', this.transformRotate(180));
+
+    });
   }
 
-  createChart() {
-    const g = this.prepareG('chart');
-    this.pie(g);
-  }
-
-  private prepareG(id: string) {
-    d3.selectAll(`#${id} svg`).remove();
+  private prepareSVG(id: string) {
     return d3.select(`#${id}`)
       .append('svg')
       .attr('width', '100%')
       .attr('height', '100%')
       .attr('viewBox', '0 0 ' + this.width + ' ' + this.height)
-      .attr('preserveAspectRatio', 'xMidYMid')
-      .append('g')
-      .attr('transform', 'translate(' + this.width / 2 + ',' + this.height / 2 + ')');
+      .attr('preserveAspectRatio', 'xMidYMid');
+
   }
 
   private pie(g) {
@@ -60,8 +116,7 @@ export class ChartComponent implements OnInit {
       .append('path')
       .attr('d', d3.arc()
         .innerRadius(this.innerRadius)
-        .outerRadius(this.outerRadius)
-      )
+        .outerRadius(this.outerRadius))
       .attr('fill', (d: { data: { key: string, value: Value } }): string => {
         return d.data.value.color;
       });
